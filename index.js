340eff3093ac0687330ad059e3ecc49b16c7a8e8
anya litvinova <anya.s.litvinova@gmail.com>
2019-07-22 20:36:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -57,17 +57,11 @@ function total() {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber === undefined) {
-    return ("Sorry, we don't have a credit card on file for you.");
-  } else {
-  
-    var sum = total();
-    if (sum) {
-    console.log(`Your total cost is $${sum}, which will be charged to the card ${cardNumber}`);
-    } else {
-      console.log("Your shopping cart is empty.")
-    }
+  if (cardNumber) {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
     cart = [];
     return cart;
+  } else {
+    return console.log("Sorry, we don't have a credit card on file for you.");
   }
 }
