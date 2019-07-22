95f6a6bf2b63fffd9410daf6a6369d8c759131fd
anya litvinova <anya.s.litvinova@gmail.com>
2019-07-22 20:40:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,6 +32,16 @@ function viewCart() {
   }
 }
 
+for (var i = 0; i < cart.length; i++) {
+    if (cart[i].hasOwnProperty(item)) {
+      cart.splice(i, 1);
+      return cart;
+    }
+  }
+  return console.log("That item is not in your cart.");
+}
+
+
 function total() {
   if (cart.length !== 0) {
     var sum = 0;
