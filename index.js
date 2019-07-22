50e5f6815da67ac6c1ea2239bafbd4cc3dc75c95
anya litvinova <anya.s.litvinova@gmail.com>
2019-07-22 20:39:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ function setCart(c) {
 }
 
   function addToCart(item) {
-    var itemPrice = Math.floor( Math.random(1, 101) * 100 );
- var newItem = {[itemName]: itemPrice };
- cart.push(newItem);
- console.log(`${itemName} has been added to your cart.`);
- return cart;
-}
+  var price = Math.floor(Math.random() * 100);
+  cart.push({item : price});	 
+  console.log(item +' has been added to your cart.');	
+  return cart;	  
+}	
+
 
 function viewCart() {
   if (!cart.length) {
